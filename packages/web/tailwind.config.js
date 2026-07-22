553a7df7e5965d02d2c43e1e5c1a905d5375df4e
roddyalfred-gif <roddyalfred@gmail.com>
2026-07-22 00:27:57 -0400

    Add Tailwind CSS configuration

---
+++ packages/web/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#00a8ff',
+        secondary: '#7c3aed',
+        dark: '#0a0e27',
+      },
+    },
+  },
+  plugins: [],
+}
