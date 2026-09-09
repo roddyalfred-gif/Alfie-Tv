f5a6b4039f03253323ccfff00f58fc11bf0af342
roddyalfred-gif <roddyalfred@gmail.com>
2026-09-09 08:01:30 -0400

    Export streaming diagnostics types

--- packages/core/src/streaming/index.ts
+++ packages/core/src/streaming/index.ts
@@ -1,3 +1,4 @@
 export { StreamingEngine } from './engine';
+export type { StreamingDiagnostics, StreamingStatus } from './engine';
 export { StreamQuality, StreamFormat } from './types';
 export type { StreamConfig, StreamMetadata, StreamEvent } from './types';
