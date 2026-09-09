60272707118caf00df3adc4ecbd22884384adcb7
roddyalfred-gif <roddyalfred@gmail.com>
2026-09-09 08:57:50 -0400

    feat: improve Android TV player controls and aspect ratio

--- apps/android/app/src/main/java/com/alfietv/player/MainActivity.kt
+++ apps/android/app/src/main/java/com/alfietv/player/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.alfietv.player
 
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
+import androidx.media3.ui.AspectRatioFrameLayout
 import androidx.media3.ui.PlayerView
 
 class MainActivity : ComponentActivity() {
@@ -12,18 +14,59 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
         playerView = PlayerView(this).apply {
             useController = true
             setShowBuffering(PlayerView.SHOW_BUFFERING_WHEN_PLAYING)
+            resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FIT
+            isFocusable = true
+            isFocusableInTouchMode = true
         }
         setContentView(playerView)
+
         alfiePlayer = AlfiePlayer(this)
         alfiePlayer.attach(playerView)
+
         val url = intent.getStringExtra("stream_url")
-        if (!url.isNullOrBlank()) alfiePlayer.play(url, intent.getStringExtra("title") ?: "Alfie TV")
+        if (!url.isNullOrBlank()) {
+            alfiePlayer.play(url, intent.getStringExtra("title") ?: "Alfie TV")
+        }
     }
 
-    override fun onStart() { super.onStart(); playerView.requestFocus() }
-    override fun onStop() { super.onStop(); alfiePlayer.stop() }
-    override fun onDestroy() { alfiePlayer.release(); super.onDestroy() }
+    override fun dispatchKeyEvent(event: KeyEvent): Boolean {
+        if (event.action == KeyEvent.ACTION_DOWN) {
+            when (event.keyCode) {
+                KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE -> {
+                    if (alfiePlayer.player.isPlaying) alfiePlayer.player.pause()
+                    else alfiePlayer.player.play()
+                    return true
+                }
+                KeyEvent.KEYCODE_MEDIA_PLAY -> {
+                    alfiePlayer.player.play()
+                    return true
+                }
+                KeyEvent.KEYCODE_MEDIA_PAUSE -> {
+                    alfiePlayer.player.pause()
+                    return true
+                }
+            }
+        }
+        return super.dispatchKeyEvent(event)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        playerView.requestFocus()
+        alfiePlayer.player.playWhenReady = true
+    }
+
+    override fun onStop() {
+        alfiePlayer.stop()
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        alfiePlayer.release()
+        super.onDestroy()
+    }
 }
