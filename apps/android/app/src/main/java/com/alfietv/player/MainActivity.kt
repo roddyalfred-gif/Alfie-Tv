0dc3a251c49d762f2dae635a10cfe74639f4d86c
roddyalfred-gif <roddyalfred@gmail.com>
2026-09-09 08:11:01 -0400

    Connect channel selection to the Media3 player

--- apps/android/app/src/main/java/com/alfietv/player/MainActivity.kt
+++ apps/android/app/src/main/java/com/alfietv/player/MainActivity.kt
@@ -12,29 +12,18 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
-
         playerView = PlayerView(this).apply {
             useController = true
             setShowBuffering(PlayerView.SHOW_BUFFERING_WHEN_PLAYING)
         }
         setContentView(playerView)
-
         alfiePlayer = AlfiePlayer(this)
         alfiePlayer.attach(playerView)
+        val url = intent.getStringExtra("stream_url")
+        if (!url.isNullOrBlank()) alfiePlayer.play(url, intent.getStringExtra("title") ?: "Alfie TV")
     }
 
-    override fun onStart() {
-        super.onStart()
-        playerView.requestFocus()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        alfiePlayer.stop()
-    }
-
-    override fun onDestroy() {
-        alfiePlayer.release()
-        super.onDestroy()
-    }
+    override fun onStart() { super.onStart(); playerView.requestFocus() }
+    override fun onStop() { super.onStop(); alfiePlayer.stop() }
+    override fun onDestroy() { alfiePlayer.release(); super.onDestroy() }
 }
