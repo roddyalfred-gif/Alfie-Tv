f03fbe0b282dcd5f1c1dbba610a0e8b72dae9f8f
roddyalfred-gif <roddyalfred@gmail.com>
2026-09-09 08:05:35 -0400

    feat(android): add native Alfie TV player activity

---
+++ apps/android/app/src/main/java/com/alfietv/player/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.alfietv.player
+
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.activity.ComponentActivity
+import androidx.media3.ui.PlayerView
+
+class MainActivity : ComponentActivity() {
+    private lateinit var playerView: PlayerView
+    private lateinit var alfiePlayer: AlfiePlayer
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
+        playerView = PlayerView(this).apply {
+            useController = true
+            setShowBuffering(PlayerView.SHOW_BUFFERING_WHEN_PLAYING)
+        }
+        setContentView(playerView)
+
+        alfiePlayer = AlfiePlayer(this)
+        alfiePlayer.attach(playerView)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        playerView.requestFocus()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        alfiePlayer.stop()
+    }
+
+    override fun onDestroy() {
+        alfiePlayer.release()
+        super.onDestroy()
+    }
+}
