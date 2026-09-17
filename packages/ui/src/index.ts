13e9df0e02ad7bd568a7ce75bbe91a38c67b77cd
roddyalfred-gif <roddyalfred@gmail.com>
2026-09-17 13:48:37 -0400

    feat(ui): export adaptive interface system

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,11 @@
 export { SplashScreen } from './components/SplashScreen';
 export { VideoPlayer } from './components/VideoPlayer';
 export { ChannelList } from './components/ChannelList';
+export { AdaptiveNavigation, AdaptiveShell } from './adaptive/AdaptiveShell';
+export type {
+  AdaptiveNavigationItem,
+  AdaptiveNavigationProps,
+  AdaptiveShellProps,
+  AlfieInputMode,
+  AlfieViewportMode,
+} from './adaptive/AdaptiveShell';
