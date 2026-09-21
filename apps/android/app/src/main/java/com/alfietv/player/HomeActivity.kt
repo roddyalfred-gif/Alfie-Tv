333d44b8081aec782803612fc576ca7a7ca1d047
roddyalfred-gif <roddyalfred@gmail.com>
2026-09-21 14:20:27 -0400

    fix: make home adaptive on landscape phones

--- apps/android/app/src/main/java/com/alfietv/player/HomeActivity.kt
+++ apps/android/app/src/main/java/com/alfietv/player/HomeActivity.kt
@@ -68,8 +68,7 @@ class HomeActivity : androidx.activity.ComponentActivity() {
     private fun buildHome() {
         val widthDp = resources.configuration.screenWidthDp
         val heightDp = resources.configuration.screenHeightDp
-        val compact = widthDp < 600
-        val tablet = widthDp in 600..899
+        // Landscape phones can report a wide screen width, so classify by the shorter dimension too.\n        val compact = widthDp < 600 || heightDp < 500\n        val tablet = widthDp in 600..899
         val wide = widthDp >= 900
         val contentHorizontalPadding = when {
             widthDp < 360 -> 10
